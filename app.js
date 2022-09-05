e37f1ded9222fb8b5ca29a8c4ba1ce98875f9fb6
shaikh <shaikh99mubashir>
2022-09-05 17:02:02 +0500

    updated

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//inilitize nested object
 let mobiles = {
   iphone: {
     v14: {
@@ -70,20 +71,29 @@ let mobiles = {
 };
 
 document.write(`<h1>Nested Object</h1>`)
+//only one nested object is printed
+//replicate nested obj through spread operator
 let nestedObj = { ...mobiles.infinix.hot13 };
 console.log(nestedObj);
+//get keys
 let value1 = Object.keys(nestedObj)
+//get values
 let value2 = Object.values(nestedObj)
-document.write(value1+"<br>");
-document.write(value2);
+//display keys and values
+document.write(value1+"<br>"+value2);
+
 
 document.write(`<h1>Nested Array</h1>`)
+//inilitize nested Array
 let nestedArray = [
 ['iphone',['v14',['ram:2gb','memory:2gb','price:265161']]],
 ['infinix',['hot10',['ram:2gb','memory:2gb','price:56188']]],
 ['samsung',['A32',['ram:2gb','memory:2gb','price:268561']]],
 ['tecno',['s8',['ram;2gb','memory:2gb','price:2661']]],
             ]
+            //last index of each array
+      //replicate nested obj through spread operator
 let copy = [...nestedArray];
+//display nested array
 document.write(copy[0][1][1] + "<br>"+copy[1][1][1]+ "<br>"+copy[2][1][1] + "<br>"+copy[3][1][1] + "<br>");
 
